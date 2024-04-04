89dde21e854fa75125a9fd3d69b2eca60beaf16d
Jicxerr <jtj9928@students.uc-bcf.edu.ph>
2024-04-04 13:12:24 +0800

    second commits

---
+++ activities/src/main/java/org/jtj/point/Point.java
@@ -0,0 +1,45 @@
+package org.jtj.point;
+
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(){
+
+    }
+
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
+
+    public void setX(int x){
+        this.x = x;
+    }
+
+    public void setY(int y){
+        this.y = y;
+    }
+
+    public double distance(Point a){
+        return distance(a.x, a.y);
+    }
+
+    public double distance(){
+        return distance(0,0);
+    }
+
+    public double distance(int x, int y){
+        int xd = this.x - x;
+        int yd = this.y - y;
+        return Math.sqrt(xd * xd + yd * yd);
+    }
+}
